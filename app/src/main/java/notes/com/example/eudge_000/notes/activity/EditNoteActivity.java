7c7c214b9ac32cbcdc6e17c10b60aa57d4e2832e
Женя Тригуб < >
2016-11-14 18:27:07 +0200

    added view to activity

--- app/src/main/java/notes/com/example/eudge_000/notes/activity/EditNoteActivity.java
+++ app/src/main/java/notes/com/example/eudge_000/notes/activity/EditNoteActivity.java
@@ -4,11 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import notes.com.example.eudge_000.notes.R;
 
 public class EditNoteActivity extends AppCompatActivity {
+
+    @BindView(R.id.title_text_view)
+    protected TextView mTitleTextView;
+    @BindView(R.id.edit_note_text_view)
+    protected TextView mEditNoteTextView;
+    @BindView(R.id.create_button)
+    protected Button mButtonCreate;
 
     public static Intent newInstance(Context context){
         Intent intent = new Intent(context, EditNoteActivity.class);
@@ -21,6 +32,7 @@ public class EditNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
+        ButterKnife.bind(this);
         String extraSting = getIntent().getStringExtra(DATA_KEY);
         Toast.makeText(this, extraSting, Toast.LENGTH_SHORT).show();
     }
